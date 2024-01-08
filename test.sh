d703f7b248a85e1a3fd7df63b7e4b414cb04d018
Aleksandar Radišić <73358934+AleksandarRadisic@users.noreply.github.com>
2024-01-08 02:46:35 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-version=$(git tag -l 'v*' --sort=-version:refname | head -n 1)
-echo "Latest tag: $version"
-
-if [ -z "$version" ]; then
-  version="v1"
-else
-  version=$(echo $version | cut -c 2- | awk -F '.' '{print "v" $1+1}')
-fi
-echo "Calculated version: $version"
